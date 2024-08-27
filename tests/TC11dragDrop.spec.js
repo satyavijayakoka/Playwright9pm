5505cfc833900bdd000bca0210a0c8367f188a5d
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-08-27 14:29:12 -0400

    add

---
+++ tests/TC11dragDrop.spec.js
@@ -0,0 +1,21 @@
+const { test, expect } = require('@playwright/test')
+const exp = require('constants')
+
+test('drag and drop using inbuild command', async ({ page }) => {
+    await page.goto('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+    let rome = await page.locator('#box6')
+    let italy = await page.locator('#box106')
+    await rome.dragTo(italy)
+    expect(rome).toHaveAttribute('style', 'visibility: visible; background-color: rgb(0, 255, 0);')
+    await page.waitForTimeout(4000)
+})
+
+test.only('verify drag and drop using mouse actions', async ({ page }) => {
+    await page.goto('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+    await page.locator('#box6').hover()
+    await page.mouse.down()
+    await page.locator('#box106').hover()
+    await page.mouse.up()
+    await expect(await page.locator('#box6')).toHaveAttribute('style','visibility: visible; background-color: rgb(0, 255, 0);')
+    await page.waitForTimeout(4000)
+})
